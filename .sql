fad5ae77a0f62005a947f2693c3bf57d508e7e20
ElmiraSargsyann <elmira.sargsyann@gmail.com>
2026-02-10 19:15:24 +0400

    Add task 4 from session 4

--- .sql
+++ .sql
@@ -205,3 +205,33 @@ WHERE year = 2023
 GROUP by category
 HAVING COUNT(transaction_id) > 100
 ORDER BY total_revenue DESC;
+
+-- Task 3 | City-Level Activity Analysis (COUNT + HAVING + CASE)
+SELECT
+	city,
+	COUNT(transaction_id) AS transaction_count,
+	CASE
+		WHEN COUNT(transaction_id) >= 4 THEN 'High Activity'
+		WHEN COUNT(transaction_id) BETWEEN 2 AND 4 THEN 'Medium Activity'
+		ELSE 'Low Activity'
+	END AS activity_level
+FROM sales_analysis
+WHERE year = 2023
+GROUP BY city
+HAVING COUNT(transaction_id) > 1
+ORDER BY transaction_count DESC;
+
+-- Task 4 | Discount Behavior Analysis (CASE + HAVING)
+SELECT
+	category,
+	SUM(total_sales) AS total_revenue,
+	AVG(discount) AS avg_discount,
+	CASE
+		WHEN AVG(discount) > 0.40 THEN 'High Discount'
+		WHEN AVG(discount) BETWEEN 0.20 AND 0.40 THEN 'Moderate Discount'
+		ELSE 'Low or No Discount'
+	END AS discount_category
+FROM sales_analysis
+GROUP BY category
+HAVING COUNT(*) > 5
+ORDER BY SUM(total_sales) DESC;
